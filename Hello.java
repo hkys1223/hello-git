8fa5f50536a7e6c555511af02ab84419131b8e92
Kwon Leegun <hkys1223@hansung.ac.kr>
2025-10-17 13:27:56 +0000

    C4: modify Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("love wins all");
+        System.out.println("prema");
     }   
 }
